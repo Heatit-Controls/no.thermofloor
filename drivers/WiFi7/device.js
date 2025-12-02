a3a5b30837527fa3912cda8639e037aa42656686
dolby78 <orvegsund@gmail.com>
2025-12-02 14:29:58 +0100

    Turned off debug

--- drivers/WiFi7/device.js
+++ drivers/WiFi7/device.js
@@ -11,7 +11,7 @@ module.exports = class MyDevice extends Homey.Device {
      */
     async onInit() {
         this.log('WiFi device has been initialized');
-        this.isDebug = true;
+        this.isDebug = false;
         this.deviceIsDeleted = false;
         this.registerCapabilityListener('target_temperature', async (value) => {
             this.debug("Changed temp", value);
