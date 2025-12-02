f61e0f60fe4beae913b996cc38076de233d5d503
dolby78 <orvegsund@gmail.com>
2025-12-02 12:08:58 +0100

    New Network Discovery

--- drivers/WiFi7/driver.js
+++ drivers/WiFi7/driver.js
@@ -1,10 +1,10 @@
 'use strict';
 const Homey = require('homey');
 const http = require('node:http');
-const net = require('node:net')
+const net = require('node:net');
+const os = require('node:os');
 
 module.exports = class MyDriver extends Homey.Driver {
-
     /**
      * onInit is called when the driver is initialized.
      */
@@ -19,22 +19,21 @@ module.exports = class MyDriver extends Homey.Driver {
     async onPairListDevices() {
 
         let discoveryResults = await this.scanNetwork();
-
         let compatibleDevices = [];
 
-        await discoveryResults.forEach(async ip => {
+        for (const item of discoveryResults) {
             compatibleDevices.push(
                 {
-                    name: "WiFi7 IP" + ip,
+                    name: "WiFi7 IP" + item.Ip,
                     data: {
-                        id: "WiFi7-Thermostat" + Math.floor(Math.random() * 1000000000000),
+                        id: "WiFi7-Thermostat" + item.Mac,
                     },
                     store: {
-                        address: ip
+                        address: item.Ip
                     }
                 }
             );
-        });
+        }
 
         if (compatibleDevices.length == 0) {
             return [
@@ -52,23 +51,24 @@ module.exports = class MyDriver extends Homey.Driver {
     }
 
     async scanNetwork() {
-        const baseIp = '192.168.1.'; // Replace with your network's base IP
+        const baseIp = this.getBaseIpAddress(); //'192.168.1.'; Replace with your network's base IP
         const out = [];
         for (let i = 1; i <= 254; i++) {
             const ip = baseIp + i;
             const isOnline = await this.checkTcpConnection(ip, 80); // Check port 80
             if (isOnline) {
                 this.log(`Device found at: ${ip}`);
-                if (await this.isWiFiThermostat(ip)) {
-                    out.push(ip);
-                    this.log('Yes is WiFi Thermostat')
+                let data = await this.getWiFiThermostatData(ip);
+                if (data.IsWiFi7Thermostat) {
+                    out.push({ "Ip": ip, "Mac": data.Mac });
+                    this.log('Yes is WiFi7 Thermostat. Mac: ' + data.Mac)
                 }
             }
         }
         return out;
     }
 
-    checkTcpConnection(hostname, port = 80, timeout = 80) {
+    async checkTcpConnection(hostname, port = 80, timeout = 80) {
         return new Promise((resolve) => {
             const socket = net.createConnection(port, hostname);
             socket.setTimeout(timeout);
@@ -87,11 +87,7 @@ module.exports = class MyDriver extends Homey.Driver {
         });
     }
 
-    async isNotWiFiThermostat(ip) {
-        return await !this.isWiFiThermostat(ip);
-    }
-
-    async isWiFiThermostat(ip) {
+    async getWiFiThermostatData(ip) {
 
         this.log('isNotWiFiThermostat IP ' + ip);
 
@@ -115,19 +111,48 @@ module.exports = class MyDriver extends Homey.Driver {
                         const parsedData = JSON.parse(rawData);
                         if (parsedData.model === "Heatit WiFi7") {
                             this.log('IsWiFi7Thermostat true');
-                            resolve(true);
+                            resolve({"IsWiFi7Thermostat": true, "Mac": parsedData.network.mac });
                         } else {
-                            resolve(false);
+                            resolve({"IsWiFi7Thermostat": false});
                         }
                     } catch (e) {
-                        resolve(false);
+                        resolve({"IsWiFi7Thermostat": false});
                     }
                 });
 
             }).on('error', (e) => {
                 this.log('isWiFiThermostat false');
-                resolve(false);
+                resolve({"IsWiFi7Thermostat": false});
             });
         });
+    }
+
+    getBaseIpAddress() {
+        const networkInterfaces = os.networkInterfaces();
+        let localIp = '192.168.1.1';
+
+        for (const devName in networkInterfaces) {
+            const iface = networkInterfaces[devName];
+            for (let i = 0; i < iface.length; i++) {
+                const alias = iface[i];
+                if (alias.family === 'IPv4' && alias.address !== '127.0.0.1' && !alias.internal) {
+                    localIp = alias.address; 
+                    break; // Found a suitable IPv4 address, exit inner loop
+                }
+            }
+            if (localIp !== '192.168.1.1') {
+                break; // Found a suitable IPv4 address, exit outer loop
+            }
+        }
+
+        const octets = localIp.split('.');
+
+        if (octets.length === 4) {
+            return octets[0] + '.' + octets[1] + '.' + octets[2] + '.';
+        } else {
+            return '192.168.1.';
+        }
+
+        
     }
 };
