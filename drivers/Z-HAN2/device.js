c65d8b66b569577c071d75b630cfde7ffb65d673
Frankenweeni <fkortenhoeven@gmail.com>
2026-01-19 15:16:48 +0100

    testing HAN2 changes

--- drivers/Z-HAN2/device.js
+++ drivers/Z-HAN2/device.js
@@ -11,42 +11,58 @@ class ZHAN2Device extends ZwaveDevice {
     // **Enables printing nodes
     // this.printNode();
 
-    this.registerCapability('meter_power', 'METER');
+    this.registerCapability('meter_power', 'METER', {
+      report: 'METER_REPORT',
+      reportParser: (report) => {
+        if (report && report.hasOwnProperty('Properties1') && (report['Properties1']['Meter Type'] & 0x01) === 0x01) {
+          if (report['Properties2'] && (report['Properties2']['Scale bits 10'] === 0x00)) {
+            return report['Meter Value (Parsed)'];
+          }
+        }
+        return null;
+      },
+    });
+
+    this.registerCapability('meter_power.export', 'METER', {
+      get: 'METER_GET',
+      getOpts: {
+        getOnStart: true,
+      },
+      getParser: () => {
+        return {
+          'Rate Type': 'Export',
+          'Scale': 1,
+        };
+      },
+      report: 'METER_REPORT',
+      reportParser: (report) => {
+        if (report && report.hasOwnProperty('Properties1') && (report['Properties1']['Meter Type'] & 0x01) === 0x01) {
+          if (report['Properties2'] && report['Properties2']['Scale bits 10'] === 0x01) {
+            return report['Meter Value (Parsed)'];
+          }
+        }
+        return null;
+      },
+    });
+
     this.registerCapability('measure_power', 'METER');
     this.registerCapability('measure_voltage', 'METER');
     this.registerCapability('measure_current', 'METER');
     this.registerCapability('measure_temperature', 'SENSOR_MULTILEVEL');
-    this.registerAccumulatedProduction();
 
     this.registerCapabilityListener('button.reset_meter', async () => {
       let commandClassMeter = null;
       commandClassMeter = this.getCommandClass('METER');
-      if(commandClassMeter && commandClassMeter.hasOwnProperty('METER_RESET')){
+      if (commandClassMeter && commandClassMeter.hasOwnProperty('METER_RESET')) {
         const result = await commandClassMeter.METER_RESET({});
-        if(result !== 'TRANSMIT_COMPLETE_OK') throw result;
+        if (result !== 'TRANSMIT_COMPLETE_OK') throw result;
       }
-      else{
+      else {
         throw new Error('Reset meter not supported');
       }
     });
-    }
 
     //listener for kVAh
-    registerAccumulatedProduction() {
-      this.registerCapability('accumulated_production', 'METER', {
-          getOpts: {
-              getOnStart: true,
-          },
-          report: 'METER_REPORT',
-          reportParser: (report) => {
-              if (report && report.Properties2 && report.Properties2['Scale bits 10'] === 0x01) {
-                  let parsedValue = Number(report['Meter Value (Parsed)']);
-                  //this.log('kVAh: ', parsedValue);
-                  return !isNaN(parsedValue) ? parsedValue : null;
-              }
-              return null;
-          }
-      });
   
       this.log('Z-HAN2 has been initialized');
       this.setAvailable().catch(this.error);
