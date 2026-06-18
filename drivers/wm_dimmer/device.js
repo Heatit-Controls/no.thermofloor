15ab0822372e36cdd101155f4410bb582dddbbdf
dolby78 <orvegsund@gmail.com>
2026-06-18 10:57:29 +0200

    Added driver parameters

--- drivers/wm_dimmer/device.js
+++ drivers/wm_dimmer/device.js
@@ -1,6 +1,7 @@
 'use strict';
-
 const Homey = require('homey');
+const http = require('node:http');
+const util = require('../../lib/util');
 
 module.exports = class MyDevice extends Homey.Device {
 
@@ -8,8 +9,41 @@ module.exports = class MyDevice extends Homey.Device {
    * onInit is called when the device is initialized.
    */
   async onInit() {
-    this.log('MyDevice has been initialized');
+      this.log('WM Dimmer has been initialized');
+      this.isDebug = true;
+      this.deviceIsDeleted = false;
+
+      this.registerCapabilityListener('onoff', async (value) => {
+          this.debug("Changed On/Off", value);
+          if (value) {
+              this.setDimmerOn();
+          } else {
+              this.setDimmerOff();
+          }
+      });
+
+      await this.loadSettings();
   }
+
+    async loadSettings() {
+        if (this.getStore().address != null) {
+
+            if (!util.isValidIpAddress(this.getSettings().IPaddress.trim())) {
+                await this.setSettings({
+                    IPaddress: this.getStore().address,
+                });
+            }
+
+            this.IPaddress = this.getStore().address;
+        } else {
+            this.IPaddress = this.getSettings().IPaddress.trim();
+        }
+        this.MaxReconnactionTrys = 5;
+        this.ReconnactionTry = 1;
+        this.MACaddress = this.getSettings().MACaddress.trim().toUpperCase();
+        this.MACaddressIsValid = util.isValidMACAddress(this.MACaddress);
+        this.ReportInterval = this.getSettings().interval;
+    }
 
   /**
    * onAdded is called when the user adds the device, called just after pairing.
@@ -17,6 +51,59 @@ module.exports = class MyDevice extends Homey.Device {
   async onAdded() {
     this.log('MyDevice has been added');
   }
+
+    debug(msg) {
+        if (this.isDebug) {
+            this.log(msg);
+        }
+    }
+
+    async setDimmerOn() {
+        const postData = JSON.stringify({
+            'dimmerState': 1,
+        });
+        await this.setParameters(postData);
+    }
+
+    async setDimmerOff() {
+        const postData = JSON.stringify({
+            'dimmerState': 0,
+        });
+        await this.setParameters(postData);
+    }
+
+    async setParameters(postData) {
+        this.debug('setParameters');
+
+        const options = {
+            hostname: this.IPaddress,
+            port: 80,
+            path: '/api/parameters',
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Content-Length': Buffer.byteLength(postData),
+            },
+        };
+
+        const req = http.request(options, (res) => {
+            this.debug(`STATUS: ${res.statusCode}`);
+            this.debug(`HEADERS: ${JSON.stringify(res.headers)}`);
+            res.setEncoding('utf8');
+            res.on('data', (chunk) => {
+                this.debug(`BODY: ${chunk}`);
+            });
+            res.on('end', () => {
+            });
+        });
+
+        req.on('error', (e) => {
+            this.log(`problem with request: ${e.message}`);
+        });
+
+        req.write(postData);
+        req.end();
+    }
 
   /**
    * onSettings is called when the user updates the device's settings.
@@ -27,7 +114,14 @@ module.exports = class MyDevice extends Homey.Device {
    * @returns {Promise<string|void>} return a custom message that will be displayed
    */
   async onSettings({ oldSettings, newSettings, changedKeys }) {
-    this.log('MyDevice settings where changed');
+      this.log("My heatit WM Dimmer device settings where changed");
+
+      if (!util.isValidIpAddress(newSettings.IPaddress)) {
+          throw new Error('Invalid IP address!')
+      }
+
+      this.IPaddress = newSettings.IPaddress;
+      this.ReportInterval = newSettings.interval;
   }
 
   /**
